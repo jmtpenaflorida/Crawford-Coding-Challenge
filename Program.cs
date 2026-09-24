2a71fb155a3e8231b20bbe461f943c5974385409
jmtpenaflorida <jmtpenaflorida@gmail.com>
2026-09-24 11:50:02 +0800

    Simple file validation

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IFileProcessor, FileProcessor>();
 builder.Services.AddScoped<IApplicantService, ApplicantService>();
+builder.Services.AddScoped<IFileValidator, FileValidator>();
 
 builder.Services.AddAuthentication("ApiKey")
     .AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>("ApiKey", _ => { });
@@ -18,12 +19,18 @@ app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World inside docker!!!").RequireAuthorization();
 
-app.MapPost("/applicants", async ([FromForm] IFormCollection files, IApplicantService service) =>
+app.MapPost("/applicants", async ([FromForm] IFormCollection files, IApplicantService service, IFileValidator validator) =>
 {
     var results = new List<object>();
 
     foreach (var file in files.Files)
     {
+        if (!validator.IsValid(file))
+        {
+            return Results.BadRequest(
+                $"File '{file.FileName}' is not a JSON file.");
+        }
+
         results.AddRange(await service.ProcessApplicants(file));
     }
 
@@ -36,3 +43,16 @@ app.MapPost("/applicants", async ([FromForm] IFormCollection files, IApplicantSe
 .DisableAntiforgery();
 
 app.Run();
+
+public interface IFileValidator
+{
+    bool IsValid(IFormFile file);
+}
+
+public class FileValidator : IFileValidator
+{
+    public bool IsValid(IFormFile file)
+    {
+        return file.ContentType == "application/json";
+    }
+}
